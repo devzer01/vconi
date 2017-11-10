540be1163a0a29ddbdd6861e090cc1e6679ce9e5
devzer01 <V8UAEuP09X>
2017-11-11 03:23:20 +0700

    utf8 hand crafting :) but reverse atm

--- test/libs_test.c
+++ test/libs_test.c
@@ -1,4 +1,24 @@
 //
 // Created by nayana on 11/11/17.
 //
+#include "vconi.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdint.h>
 
+int main() {
+    int32_t codepoint = 0xD85;
+    unsigned char *buffer = (unsigned char *) malloc(sizeof(unsigned char ) * 4);
+    const unsigned char *letter = getuchar(codepoint, buffer);
+    printf("%s\n", letter);
+    return 0;
+}
