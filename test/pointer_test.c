bb09268700fcf4e86c4f7028b78a741858d356e8
nayana <nayana@corp-gems.com>
2017-11-26 15:24:12 +0700

    before refactor

--- test/pointer_test.c
+++ test/pointer_test.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
 #include <stdlib.h>
 
 
 int main()
 {
-	unsigned int **matrix = malloc(sizeof(unsigned int *) * 10);
-	unsigned int row = 0, col = 0;
+	unsigned char *data = "abcdef";
+	unsigned char **matrix = malloc(sizeof(unsigned char *) * 2);
+	*matrix = malloc(sizeof(unsigned char) * 3);
+	//memcpy(matrix, data, 6);
+	*(matrix)   = (data);
+	*(matrix+1) = (data+3);
+
+	for (int i = 0; i < 2; i++) {
+		for (int x = 0; x < 3; x++) {
+			printf("%c ", matrix[i][x]);
+		}
+	}
+	printf("%c ", matrix[0][0]);
+/*
+	*(matrix) = malloc(sizeof(unsigned int) * 1);
+	*(matrix+1) = malloc(sizeof(unsigned int) * 2);
+	*(matrix+2) = malloc(sizeof(unsigned int) * 3);
+	*(matrix+4) = malloc(sizeof(unsigned int) * 4);
+
+	*(*(matrix)) = 0xFAFAFAFA;
+	*(*(matrix+1)) = 0xFFFFFFFF;
+	*(*(matrix+1)+1) = 0xFFFFFFFA;
+
+	printf("%d \n", **matrix+1);*/
+
+
+	/*unsigned int row = 0, col = 0;
 	while (row < 10) {
-		*(matrix+row) = malloc(sizeof(unsigned int) * 10);
+		*(matrix+row) = malloc(sizeof(unsigned int) * (row + 1));
 		while (col < 10) {
 			*(*(matrix+row)+col) = col * row;
 			col++;
@@ -17,6 +42,6 @@ int main()
 		col = 0;
 	}
 	while (row > 0) free(*(matrix + --row));
-	free(matrix);
+	free(matrix);*/
 	return 0;
 }
